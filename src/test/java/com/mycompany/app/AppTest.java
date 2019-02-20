e00d4a92332ffbb09e46266ac4e65c7040ea62aa
ramakumard77 <ramakumar.d@gmail.com>
2019-02-20 13:09:17 +0530

    updated

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,35 +3,12 @@ package com.mycompany.app;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-/**
- * Added junit plugins to get test reports - Ram
-*/
-import junit.framework.Test;  
-import junit.framework.TestCase;  
-import junit.framework.TestSuite;  
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /** 
-     * Create the test case  - Ram
-     * @param testName name of the test case 
-     */  
-    public AppTest( String testName )  
-    {  
-        super( testName );  
-    }  
-   
-    /** 
-     * @return the suite of tests being tested -Ram
-     */  
-    public static Test suite()  
-    {  
-        return new TestSuite( AppTest.class );  
-    }
-
     /**
      * Rigorous Test :-)
      */
@@ -40,6 +17,7 @@ public class AppTest
     {
         assertTrue( true );
     }
+
     /**
      * Rigorous Test 2:-) Ram
      */
